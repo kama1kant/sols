0ed5f003e4c4e7453b92c1ec57194d342fd2fa7a
Kamal <kamalkant.k4@gmail.com>
2023-08-29 06:37:44 -0700

    Updates

---
+++ precache-manifest.a2f51ff54b6e8c45caf36532a3bb068a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "52fe1a008d927721c300",
+    "url": "/static/css/main.60b1f502.chunk.css"
+  },
+  {
+    "revision": "52fe1a008d927721c300",
+    "url": "/static/js/main.52fe1a00.chunk.js"
+  },
+  {
+    "revision": "696d8cabe53664e366a6",
+    "url": "/static/js/1.696d8cab.chunk.js"
+  },
+  {
+    "revision": "bfabfdd50214c923b893",
+    "url": "/static/css/2.f873bf90.chunk.css"
+  },
+  {
+    "revision": "bfabfdd50214c923b893",
+    "url": "/static/js/2.bfabfdd5.chunk.js"
+  },
+  {
+    "revision": "d1eda814a5707aacd527",
+    "url": "/static/js/runtime~main.d1eda814.js"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "844324dc83d98980285a9ab8f3dcb5f0",
+    "url": "/static/media/Aeonik-Regular.844324dc.otf"
+  },
+  {
+    "revision": "cc7f36001e744a50727150ba93287f59",
+    "url": "/static/media/deeprecord-logo-text-white.cc7f3600.svg"
+  },
+  {
+    "revision": "bbb3885bd526d174a84b17ae9a8a964c",
+    "url": "/static/media/solyslabs-logo-white.bbb3885b.svg"
+  },
+  {
+    "revision": "7b7f65a94cc9afb2e0cff476aac9e525",
+    "url": "/static/media/mockup.7b7f65a9.svg"
+  },
+  {
+    "revision": "c7c891863eea6bd0020feb1a71f16d92",
+    "url": "/index.html"
+  }
+];
